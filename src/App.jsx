cca62df6f0b6d6f38e76cbf51031fbf42c06b967
Jade Craft <jadecraft9@gmail.com>
2025-05-13 15:22:32 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Cookies/>}/>
       <Route path='/register' element={<Register setToken={setToken} />}/>
       <Route path='/login' element={<Login />}/>
+      <Route path='/' element={<Cookies/>}/>
     </Routes>
       
       </>
